dd7e70738c1d15814312ff6473d2295c34d6b574
Neo <neo@local>
2026-05-12 12:27:49 +0200

    Use original and AEP updater labels

--- electron-app/main.js
+++ electron-app/main.js
@@ -103,7 +103,7 @@ else
 		else if (originalLatest && currentFile === originalLatest.file) currentSource = 'Swift'
 	} catch (error) {}
 	  
-	win.webContents.send('update', '<div style="position:absolute;left:50%;margin-left:-230px;top:30vh;width:460px;text-align:center;background-color:white;padding:10px;color:black">UPDATE<br>Current version '+currentSource+': '+version+'<br><button onClick="window.electronAPI.updateToLatestSwift()">Update to Swift latest: '+originalVersion+'</button><br><button onClick="window.electronAPI.updateToLatestAep()">Update to Viktor latest: '+viktorVersion+'</button><br><small>(The app will automatically restart after the update)</small><br><button onClick="document.getElementById(\'updateOverlay\').remove()">Cancel</button><br></div>');
+	win.webContents.send('update', '<div style="position:absolute;left:50%;margin-left:-230px;top:30vh;width:460px;text-align:center;background-color:white;padding:10px;color:black">UPDATE<br>Current version '+currentSource+': '+version+'<br><button onClick="window.electronAPI.updateToLatestSwift()">Update to original latest: '+originalVersion+'</button><br><button onClick="window.electronAPI.updateToLatestAep()">Update to AEP latest: '+viktorVersion+'</button><br><small>(The app will automatically restart after the update)</small><br><button onClick="document.getElementById(\'updateOverlay\').remove()">Cancel</button><br></div>');
   })
   ipcMain.on('updateToLatestSwift', async function() {
 	version = await updateSLYA(ORIGINAL_UPDATE_URL);
@@ -112,7 +112,7 @@ else
   })
   ipcMain.on('updateToLatestAep', async function() {
 	version = await updateSLYA(AEP_UPDATE_URL);
-	win.webContents.send('update', '<div style="position:absolute;left:50%;margin-left:-200px;top:40vh;width:400px;text-align:center;background-color:#eee;color:black">VIKTOR CODE UPDATED<br>Restarting ...<br></div>');
+	win.webContents.send('update', '<div style="position:absolute;left:50%;margin-left:-200px;top:40vh;width:400px;text-align:center;background-color:#eee;color:black">AEP CODE UPDATED<br>Restarting ...<br></div>');
 	setTimeout(function() { win.loadFile('app/index.html', { query: { version: version } } ) } , 2000 )
   })
 
